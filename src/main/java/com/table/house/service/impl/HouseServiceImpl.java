f0926fcf9f59a1b4fcfb3235d0ee83b42b1fda82
Gbq <990924291@qq.com>
2017-01-01 21:41:03 +0800

    1、配置文件 2、业务名称修改

--- src/main/java/com/table/house/service/impl/HouseServiceImpl.java
+++ src/main/java/com/table/house/service/impl/HouseServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @SuppressWarnings("unused")
-@Service("userService")
+@Service("houseService")
 public class HouseServiceImpl extends BaseService implements HouseService {
 	
 	@Override
